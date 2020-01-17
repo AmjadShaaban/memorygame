ee15b8ae69761e413306839b754d087f261b84d4
Amjad Shaaban <theamjad@outlook.com>
2020-01-17 08:47:09 -0500

    Updates

---
+++ precache-manifest.74fcf922e03b9a1731ea3c30e022f064.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51bd68fa1eda63af09c794f3dc862808",
+    "url": "/memorygame/index.html"
+  },
+  {
+    "revision": "918695c6268131a24083",
+    "url": "/memorygame/static/js/2.7bdddcee.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/memorygame/static/js/2.7bdddcee.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3901d62143ea3fc9c43f",
+    "url": "/memorygame/static/js/main.7fa1cc85.chunk.js"
+  },
+  {
+    "revision": "78f5ee93faeabc3158c9",
+    "url": "/memorygame/static/js/runtime-main.295be826.js"
+  }
+]);
